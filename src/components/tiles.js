0fdf6ae25161cce6cc2d9b00826340cba7a6941c
meghakarkihalli <meghakarkihalli@gmail.com>
2019-01-24 12:13:36 -0800

    Third Commit

--- src/components/tiles.js
+++ src/components/tiles.js
@@ -5,7 +5,7 @@ const Tiles = (props) => {
 	return(
 		<div className="my-list3"  onMouseEnter = {props.onMouseEnter} >
 			<img src={props.img} className="image" alt =" " /><br/>
-			<span> Title: {props.title}</span><br/>
+			<span>{props.title}</span><br/>
 		
 		
 		</div>
